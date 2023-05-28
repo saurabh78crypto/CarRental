7cccd56ed72f32a1286a5ee2c0ff3dea59608124
Saurabh Pingale <saurabhpingale93@gmail.com>
2023-05-28 23:02:11 +0530

    In progress

--- reactApp/server/db/conn.js
+++ reactApp/server/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://saurabhpingale:Saurabh%4078@cluster0.u4hh8yk.mongodb.net/CarRental?retryWrites=true&w=majority')
-    .then(() => {
-        console.log('Connection Successful');
-    }).catch((err) => console.log('Connection Unsuccessful'));
+.then(() => {
+    console.log('Connection Successful');
+}).catch((err) => console.log('Connection Unsuccessful'));
+    
